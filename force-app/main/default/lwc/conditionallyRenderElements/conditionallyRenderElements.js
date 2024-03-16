6a1c754f29544fd8cb1a379b0cdb7914f1ffd779
nkmongit <38969551+nkmongit@users.noreply.github.com>
2024-03-16 21:57:11 +0530

    conditionally rendering elements

--- force-app/main/default/lwc/conditionallyRenderElements/conditionallyRenderElements.js
+++ force-app/main/default/lwc/conditionallyRenderElements/conditionallyRenderElements.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class ConditionallyRenderElements extends LightningElement {}
+export default class ConditionallyRenderElements extends LightningElement {
+    showMe = false;
+    handleChange(event) {
+        this.showMe = event.target.checked;
+        console.log(this.showMe);
+    }
+}

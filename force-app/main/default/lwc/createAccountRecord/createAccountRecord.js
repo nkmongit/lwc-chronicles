aaf7a95e9d680e1cd963bdb973b20034c15bc1e7
nkmongit <38969551+nkmongit@users.noreply.github.com>
2024-03-17 23:21:02 +0530

    craete accounts using lwc

---
+++ force-app/main/default/lwc/createAccountRecord/createAccountRecord.js
@@ -0,0 +1,25 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import PHONE_FIELD from '@salesforce/schema/Account.Phone';
+import ANNUAL_REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
+
+export default class CreateAccountRecord extends LightningElement {
+    objectApiName = ACCOUNT_OBJECT;
+    fields = [
+        NAME_FIELD,
+        PHONE_FIELD,
+        ANNUAL_REVENUE_FIELD
+    ]
+
+    handleSuccess(event) {
+        console.log(event.detail.fields.id);
+        const toastEvent = new ShowToastEvent({
+            title: 'Account has been created successfully.',
+            message: 'Account Created',
+            variant: 'success',
+        });
+        this.dispatchEvent(toastEvent);
+    }
+}
